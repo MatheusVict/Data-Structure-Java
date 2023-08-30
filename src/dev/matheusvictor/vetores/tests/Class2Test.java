fa11fea1a0fe5b5fdb5cc2e286c924343b8c8623
MatheusVict <matheusvictorhenrique@gmail.com>
2023-08-30 15:39:32 -0300

    feat: basic vector
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/dev/matheusvictor/vetores/tests/Class2Test.java
@@ -0,0 +1,10 @@
+package dev.matheusvictor.vetores.tests;
+
+import dev.matheusvictor.vetores.Vector;
+
+public class Class2Test {
+    public static void main(String[] args) {
+        Vector vector = new Vector(5);
+
+    }
+}
