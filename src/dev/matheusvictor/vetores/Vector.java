05a92d02e72194f5711970bf76e65b4c54d40ccb
MatheusVict <matheusvictorhenrique@gmail.com>
2023-08-30 16:43:39 -0300

    feat: sequencial search
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/dev/matheusvictor/vetores/Vector.java
+++ src/dev/matheusvictor/vetores/Vector.java
@@ -1,9 +1,79 @@
 package dev.matheusvictor.vetores;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Vector {
     private String[] elements;
+    private int size;
 
     public Vector(int capacity) {
         this.elements = new String[capacity];
+        this.size = 0;
+    }
+
+    /*public void add(String element) {
+        for (int i = 0; i < this.elements.length; i++) {
+            if (elements[i] == null) {
+                elements[i] = element;
+                break;
+            }
+        }
+    }*/
+
+    /*public void add(String element) throws Exception {
+        if (this.size < this.elements.length) {
+            this.elements[this.size] = element;
+            this.size++;
+        } else {
+            throw new Exception("Vector full");
+        }
+    } */
+
+    public String search(int position) {
+        if (!(position >= 0 && position < size)) {
+            throw new IllegalArgumentException("invalid position");
+        }
+        return this.elements[position];
+    }
+    public int search(String element) {
+        for (int i=0; i<this.size; i++) {
+            if (this.elements[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean add(String element) {
+        if (this.size < this.elements.length) {
+            this.elements[this.size] = element;
+            this.size++;
+            return true;
+        }
+        return false;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+
+        for (int i=0; i<this.size-1; i++) {
+            stringBuilder.append(this.elements[i]);
+            stringBuilder.append(", ");
+        }
+
+        if (this.size > 0) {
+            stringBuilder.append(this.size-1);
+        }
+
+        stringBuilder.append("]");
+
+        return stringBuilder.toString();
     }
 }
--- src/dev/matheusvictor/vetores/tests/Class2Test.java
+++ src/dev/matheusvictor/vetores/tests/Class2Test.java
@@ -4,7 +4,17 @@ import dev.matheusvictor.vetores.Vector;
 
 public class Class2Test {
     public static void main(String[] args) {
-        Vector vector = new Vector(5);
+        Vector vector = new Vector(10);
+
+        vector.add("uepa");
+        vector.add("uepa1");
+        vector.add("uepa3");
+        vector.add("uepa4");
+
+        System.out.println(vector.getSize());
+        System.out.println(vector);
+        System.out.println(vector.search(1));
+        System.out.println(vector.search("uepa4"));
 
     }
 }
